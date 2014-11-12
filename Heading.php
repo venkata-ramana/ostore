148999d768dc88ea8730c7c4a2cd25e5425c4862
venkata-ramana <vramana98@gmail.com>
2014-11-12 14:14:52 +0530

    moving templates

--- Heading.php
+++
@@ -1,6 +0,0 @@
-<body bgcolor="grey">
-<table width="100%" border="0" cellspacing="0" cellpadding="0">
-  <tr bgcolor="grey">
-      <td><center><img src="Head.png" height="80px"></center></td>
-  </tr>
-</body>
